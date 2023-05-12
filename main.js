47a570960e8e11d7c5600083c319d006d2578c1c
Douglas Eduardo <doug.edgates@gmail.com>
2023-05-12 17:08:02 -0300

    first: Projeto completo. Inserção de nome e telefone na lista. Atualização do número de contatos na lista.

---
+++ main.js
@@ -0,0 +1,41 @@
+﻿const form = document.querySelector('.form-contato');
+const adicionar = document.querySelector('.adicionar');
+let nomes = [];
+let telefones = [];
+let linhas = '';
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    
+    adicionaContato();
+    contaContatos();
+})
+
+function adicionaContato() {
+    const inputNome = document.querySelector('.nome');
+    const inputTelefone = document.querySelector('.telefone');
+    const corpoTabela = document.querySelector('tbody');
+    
+    nomes.push(inputNome.value);
+    telefones.push(parseFloat(inputTelefone.value));
+
+    let linha = `<tr>`;
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+
+    corpoTabela.innerHTML = linhas;
+
+    console.log(nomes, nomes.length)
+}
+
+function contaContatos() {
+    const contador = document.querySelector('.numero-contatos');
+
+    contador.innerHTML = `Você tem${nomes.length} contatos.`;
+}
